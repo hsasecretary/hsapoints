d880414762052e309946d38b9b0e27471634014e
Sofia Lynch <113479969+SofiaBLynch@users.noreply.github.com>
2024-08-29 10:46:02 -0400

    Remove the points breakdown from everyone until we make a distinct cabinet page

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -9,14 +9,15 @@ function Dashboard({eboard}) {
 	return (
 		
 		<div className="formDash" >
+			
 			<Logout/>
 			<div id="dash"><h2>Dashboard</h2></div>
 			<Points />
 			<br/>
 			<Attendance />
 			<br/>
-			<CabinetPoints/>
-			<br/>
+			{/* <CabinetPoints/>
+			<br/> */}
 
 		</div>
 	);
