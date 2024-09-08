aa3b86f245321ee288a3b4baa64e6e09a8ef7e44
Sofia Lynch <113479969+SofiaBLynch@users.noreply.github.com>
2024-09-08 06:51:22 -0400

    Oops forgot to import cabinetPoints

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -2,7 +2,7 @@ import './Dashboard.css';
 import Points from './Points'
 import Attendance from './Attendance';
 import Logout from './Logout';
-// import CabinetPoints from './CabinetPoints';
+import CabinetPoints from './CabinetPoints';
 
 function Dashboard({eboard}) {
 	
